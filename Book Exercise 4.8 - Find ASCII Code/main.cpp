d0f911949d367473ed2e3022aed525efa21f922f
alexberry721 <alexberry721@gmail.com>
2016-09-12 12:33:00 -0700

    Initial Commit

---
+++ Book Exercise 4.8 - Find ASCII Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 4.8 - Find ASCII Code
+//
+//  Created by ax on 9/12/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
