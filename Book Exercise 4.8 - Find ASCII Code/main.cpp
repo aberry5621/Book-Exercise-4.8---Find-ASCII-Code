f3ca53aeb5a308f6eb573256fdf9dbf1b02b576d
alexberry721 <alexberry721@gmail.com>
2016-09-12 12:41:33 -0700

    Init commit

--- Book Exercise 4.8 - Find ASCII Code/main.cpp
+++ Book Exercise 4.8 - Find ASCII Code/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    cout << "Find Character from ASCII Code \n";
+    
+    // Data and Initialization
+
+    int input_code = 0.0;
+    char char_output = ' ';
+    
+
+    // I
+    // ASCII input, i.e.: 0XFF41
+    cout << "Enter an ASCII code: ";
+    cin >> input_code;
+    
+    // P
+    // convert input code to ascii character
+    cout << "You entered: " << input_code << endl;
+    char_output = static_cast<char>(input_code);
+    
+    // O
+    cout << "The character is: " << char_output << endl;
+    
     return 0;
 }
